f95ae699fe696e6c6652a1069b46af01a5075e19
Robbyro89 <gallery.laroche.id@gmail.com>
2018-02-01 17:28:48 +0800

    redit model

--- app/models/cour.rb
+++ app/models/cour.rb
@@ -2,5 +2,5 @@ class Cour < ApplicationRecord
   validates :titre, presence: true, length: { maximum: 50 },
                     uniqueness: true
   validates :description, presence: true, length: { maximum: 255 }
-  has_many :lesson
+  has_many :lessons
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,5 +2,5 @@ class Lesson < ApplicationRecord
   validates :titre, presence: true, length: { maximum: 50 },
                     uniqueness: true
   validates :description, presence: true, length { minimum: 5 }
-  belongs_to :cour
+  belongs_to :cours
 end
